10710177bbdfd7b207f1c2f1424a31a5897aa3be
vbnm134678 <syj1351@naver.com>
2023-01-19 11:28:36 +0900

    rename: navbar, sidebar directory구조 변경 #18

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,10 +1,51 @@
-import Header from "@/components/Navbar/Header";
+import { SetStateAction, useState } from "react";
+import { RxDashboard } from "react-icons/rx";
+import H from "./styles";
 
 const Navbar = () => {
+    const [searching, setSearching] = useState("");
+
+    const searchChange = (e: { target: { value: SetStateAction<string> } }) => {
+        setSearching(e.target.value);
+        console.log(searching);
+    };
+
     return (
-        <>
-            <Header />
-        </>
+        <H.HeadNav className="navbar">
+            <H.HeadUl>
+                <H.HeadLiitem>
+                    <div className="nav_logo">Logo</div> {/* Logo space */}
+                </H.HeadLiitem>
+                <H.HeadLiitem>
+                    <H.HeadLink to="/">workspace</H.HeadLink>
+                </H.HeadLiitem>
+                <H.HeadLiitem>
+                    <H.HeadLink to="/">recent</H.HeadLink>
+                </H.HeadLiitem>
+                <H.HeadLiitem>
+                    <H.HeadLink to="/">starred</H.HeadLink>
+                </H.HeadLiitem>
+                <H.HeadLiitem>
+                    <H.HeadCreate type="button" className="head_create">
+                        Create
+                    </H.HeadCreate>
+                </H.HeadLiitem>
+            </H.HeadUl>
+            <H.HeadUl className="nav_function">
+                <H.HeadLifunc>
+                    <input id="search" name="search" placeholder="Search" onChange={searchChange} />
+                </H.HeadLifunc>
+                <H.HeadLifunc>
+                    <RxDashboard className="alarm" />
+                </H.HeadLifunc>
+                <H.HeadLifunc>
+                    <button type="button" className="head_user">
+                        {/* <img src={user_image} alt="user_image" /> */}
+                        user_image
+                    </button>
+                </H.HeadLifunc>
+            </H.HeadUl>
+        </H.HeadNav>
     );
 };
 
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,10 +1,24 @@
-import Side from "@/components/Sidebar/Sidebar";
-
 const Sidebar = () => {
     return (
-        <>
-            <Side />
-        </>
+        <div className="sidebar">
+            <div className="side_title">
+                <ul>
+                    <li>Workspace name</li>
+                </ul>
+            </div>
+            <div>
+                <ul className="workspace_setting">
+                    <li className="workspace_board">Board</li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li className="Board1">Board 1</li>
+                    <li className="Board2">Board 2</li>
+                    <li className="Board3">Board 3</li>
+                </ul>
+            </div>
+        </div>
     );
 };
 
