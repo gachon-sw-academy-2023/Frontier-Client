6ba10e55f29386dd966e989f5643f1f8cd70e4e6
Jeonggyu98 <kimjg_98@naver.com>
2023-01-28 16:58:09 +0900

    feat: 드래그 앤 드랍  구현 중 Strict mode 해제 #26

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Landing from "@/pages/Landing";
 import Login from "@/pages/Login";
 import Home from "@/pages/Home";
 import SignUp from "./pages/SignUp";
+import Board from "./pages/Board";
 
 if (import.meta.env.MODE === "development") {
     worker.start();
@@ -29,6 +30,7 @@ const App = () => {
                         <Route path="/login" element={<Login />} />
                         <Route path="/signup" element={<SignUp />} />
                         <Route path="/homepage" element={<Home />} />
+                        <Route path="/board" element={<Board />} />
                         {/* <Route path="/workspace" component={<WorkSpace />}
                         </Route> */}
                     </Routes>
