a6f065fce59f324e2e2dac4e59ba6669b1347f64
whquddnr97 <whquddnr97@gmail.com>
2023-02-09 03:09:23 +0900

    build: mswjs/data 제거
    #57

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,10 +1,9 @@
 import Header from "@/pages/Landing/Header";
 import Hero from "@/pages/Landing/Hero";
-import database from "@/mocks/database";
 import { useEffect } from "react";
 
 const Landing = () => {
-    useEffect(() => console.log(database.card.getAll()));
+    useEffect(() => {});
     return (
         <>
             <Header />
