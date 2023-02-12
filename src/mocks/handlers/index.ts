674bcc9397692f96a7316743f893e0810fae3f4f
whquddnr97 <whquddnr97@gmail.com>
2023-02-13 05:55:54 +0900

    feat: 리스트 관련 핸들러 작성
    #63

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -2,5 +2,12 @@ import { userHandler } from "@/mocks/handlers/userHandler";
 import { authHandler } from "@/mocks/handlers/authHandler";
 import { workspaceHandler } from "@/mocks/handlers/workspaceHandler";
 import { boardHandler } from "@/mocks/handlers/boardHandler";
+import { listHandler } from "@/mocks/handlers/listHandler";
 
-export const handlers = [...userHandler, ...authHandler, ...workspaceHandler, ...boardHandler];
+export const handlers = [
+    ...userHandler,
+    ...authHandler,
+    ...workspaceHandler,
+    ...boardHandler,
+    ...listHandler,
+];
