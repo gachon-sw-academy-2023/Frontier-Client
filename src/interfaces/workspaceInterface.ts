45a48bea2e993d3828daf48a17759b1ff978abdc
whquddnr97 <whquddnr97@gmail.com>
2023-02-13 12:18:02 +0900

    feat: 워크스페이스 관련 쿼리 작성
    #66

---
+++ src/interfaces/workspaceInterface.ts
@@ -0,0 +1,25 @@
+export interface Board {
+    id: string;
+    workspaceId: string;
+    title: string;
+    createdBy: string;
+    createdAt: string;
+    modifiedAt: string;
+}
+
+export interface GetWorkspaceResBody {
+    id: string;
+    title: string;
+    createdBy: string;
+    createdAt: string;
+    modifiedAt: string;
+    board?: Board;
+}
+
+export interface PostWorkspaceReqBody {
+    title: string;
+}
+
+export interface PostWorkspaceResBody {
+    data: string;
+}
