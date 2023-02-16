74b10ac53368d395acd3458742887b22d3d78fb5
whquddnr97 <whquddnr97@gmail.com>
2023-02-16 10:35:41 +0900

    fix: 미설치 dependency 처리
    #69

--- src/pages/Card/index.tsx
+++ src/pages/Card/index.tsx
@@ -1,5 +1,4 @@
 import * as S from "@/pages/Card/styles";
-import "@toast-ui/editor/dist/toastui-editor.css";
 import CardHeader from "@/pages/Card/Header";
 import CardContent from "@/pages/Card/Content";
 import CardSidebar from "@/pages/Card/Sidebar";
