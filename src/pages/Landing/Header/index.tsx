1642b5d3d1b307edd5147ce68d5b534338a43473
whquddnr97 <whquddnr97@gmail.com>
2023-02-22 12:31:42 +0900

    fix: 랜딩 페이지 네비게이션 바 수정
    build: react-scroll 추가
    #27

--- src/pages/Landing/Header/index.tsx
+++ src/pages/Landing/Header/index.tsx
@@ -1,20 +1,65 @@
 import LogoImage from "@/assets/images/Frontier-Logo2.png";
+import { useEffect, useState } from "react";
 import * as S from "./Header.styles";
 
-const Header = () => (
-    <>
-        <S.HeaderContainer>
-            <S.LogoContainer src={LogoImage} />
-            <S.NavContainer>
-                <S.NavMenu to="homepage">Home</S.NavMenu>
-                <S.NavMenu to="#features">Features</S.NavMenu>
-                <S.NavMenu to="#stats">Stats</S.NavMenu>
-            </S.NavContainer>
-            <S.NavBtn>
-                <S.NavBtnLink to="/login">Login</S.NavBtnLink>
-            </S.NavBtn>
-        </S.HeaderContainer>
-    </>
-);
+const Header = () => {
+    const [y, setY] = useState(window.scrollY);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => setY(window.scrollY));
+        return () => {
+            window.removeEventListener("scroll", () => setY(window.scrollY));
+        };
+    }, [y]);
+    return (
+        <S.NavWrapper position={y}>
+            <S.NavInner>
+                <S.NavLogoWrapper to="home" smooth>
+                    <S.NavLogo src={LogoImage} alt="logo" />
+                </S.NavLogoWrapper>
+                <S.NavListWrapper>
+                    <S.NavList>
+                        <S.NavItem activeClass="active" to="home" spy smooth offset={-80}>
+                            Home
+                        </S.NavItem>
+                    </S.NavList>
+                    <S.NavList>
+                        <S.NavItem activeClass="active" to="services" spy smooth offset={-80}>
+                            Services
+                        </S.NavItem>
+                    </S.NavList>
+                    <S.NavList>
+                        <S.NavItem activeClass="active" to="projects" spy smooth offset={-80}>
+                            Projects
+                        </S.NavItem>
+                    </S.NavList>
+                    <S.NavList>
+                        <S.NavItem activeClass="active" to="blog" spy smooth offset={-80}>
+                            Blog
+                        </S.NavItem>
+                    </S.NavList>
+                    <S.NavList>
+                        <S.NavItem activeClass="active" to="pricing" spy smooth offset={-80}>
+                            Pricing
+                        </S.NavItem>
+                    </S.NavList>
+                    <S.NavList>
+                        <S.NavItem activeClass="active" to="contact" spy smooth offset={-80}>
+                            Contact
+                        </S.NavItem>
+                    </S.NavList>
+                </S.NavListWrapper>
+                <S.CTAWrapper>
+                    <S.LoginWrapper>
+                        <S.Login to="/login">Log in</S.Login>
+                    </S.LoginWrapper>
+                    <S.SignUpWrapper>
+                        <S.SignUp to="/signup">Get Started</S.SignUp>
+                    </S.SignUpWrapper>
+                </S.CTAWrapper>
+            </S.NavInner>
+        </S.NavWrapper>
+    );
+};
 
 export default Header;
