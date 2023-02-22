30cb5d96626f31d7af899e1d576d1dca294b0912
whquddnr97 <whquddnr97@gmail.com>
2023-02-22 17:13:21 +0900

    fix: 인증 페이지 로고 변경
    #45

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Logo from "@/assets/images/Trello-logo.png";
+import Logo from "@/assets/images/Frontier-Logo2.png";
 import RegisterFrom from "@/components/auth/RegisterForm";
 import * as S from "@/components/GlobalStyle";
 import { Title, Image } from "@/pages/SignUp/styles";
